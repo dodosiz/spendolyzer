bbfe7f148371e3c915ec861daa22a9fc4741f50a
dodosiz <t.v.asvestopoulos@gmail.com>
2025-01-16 01:20:46 +0200

    Upload CSV and show results

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Spendolyzer
+
+Analyze where you spend your money quick and easy.
+
+## What it can do
+
+All you need is a CSV file with the first column the payee (shop, person etc.) or the payer (your employer, clients etc.).
+
+For example the CSV file could look like this:
+
+```
+Payee;Amount;
+beauty shop;-50;
+employer;3000;
+car shop;-200;
+beauty shop;-150;
+```
+
+Upload this CSV and get insights on your earnings and spendings, you will see:
+
+- Your total spendings
+- Your total earnings
+- Your balance
+- A table with all aggregated spendings on descending order
+
+## I like it I want to try it out
+
+You can try it out locally by running:
+
+```
+npm install
+```
+
+And then:
+
+```
+npm run dev
+```
+
+Visit http://localhost:5173/ to view the app.
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,118 @@
+import { Button, Container, CssBaseline, Paper, styled } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import React from "react";
+import Papa from "papaparse";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const PAYEE_COLUMN = 0;
+const AMOUNT_COLUMN = 1;
+
+interface Transaction {
+  id: number;
+  payee: string;
+  amount: number;
+}
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "payee", headerName: "Payee", width: 130 },
+  {
+    field: "amount",
+    headerName: "Amount",
+    type: "number",
+    width: 90,
+  },
+];
+
+const paginationModel = { page: 0, pageSize: 50 };
+
 export function App() {
-  return <h1>Analyze your spendings</h1>;
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+  const [income, setIncome] = React.useState<number>(0);
+  const [costs, setCosts] = React.useState<number>(0);
+  const handleUpload = (files: FileList | null) => {
+    const file = files?.[0];
+    if (!file) {
+      return;
+    }
+    Papa.parse(file, {
+      complete: function (results: { data: string[][] }) {
+        const content = results.data.slice(1);
+        const totals: { [key: string]: number } = {};
+        let incomeSum = 0;
+        let costsSum = 0;
+        for (const row of content) {
+          const payee = row[PAYEE_COLUMN];
+          const amount = parseFloat(row[AMOUNT_COLUMN]);
+          if (payee && payee.length && !isNaN(amount)) {
+            if (amount < 0) {
+              totals[payee] = (totals[payee] || 0) + amount;
+              costsSum += amount;
+            } else {
+              incomeSum += amount;
+            }
+          }
+        }
+        const aggregatedTransactions = Object.entries(totals)
+          .map(([payee, amount]) => ({
+            id: 0,
+            payee,
+            amount: -amount,
+          }))
+          .sort((a, b) => b.amount - a.amount);
+        for (let i = 0; i < aggregatedTransactions.length; i++) {
+          aggregatedTransactions[i].id = i + 1;
+        }
+        setTransactions(aggregatedTransactions);
+        setIncome(incomeSum);
+        setCosts(costsSum);
+      },
+    });
+  };
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <h1>Upload CSV</h1>
+        <Button
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload CSV
+          <VisuallyHiddenInput
+            type="file"
+            onChange={(event) => handleUpload(event.target.files)}
+          />
+        </Button>
+        <h2>Income: {income}</h2>
+        <h2>Costs: {costs}</h2>
+        <h2>Balance: {income + costs}</h2>
+        <Paper sx={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={transactions}
+            columns={columns}
+            initialState={{ pagination: { paginationModel } }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+            sx={{ border: 0 }}
+          />
+        </Paper>
+      </Container>
+    </React.Fragment>
+  );
 }
